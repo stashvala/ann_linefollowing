4c7aa3852c7e968518de9f3fcc5c4ef843a5a10e
stas <user.name>
2018-06-04 11:01:27 +0200

    Moved ev3 setup to separate function

--- line_following.py
+++ line_following.py
@@ -5,29 +5,6 @@ import numpy as np
 from sklearn.neural_network import MLPRegressor
 from ev3dev.ev3 import *
 
-# GYRO SENSOR
-gs = GyroSensor()
-assert gs.connected, "Connect a gyro sensor to any sensor port"
-gs.mode = 'GYRO-ANG'
-
-# MOTORS
-motor_left = LargeMotor('outB')
-motor_right = LargeMotor('outD')
-
-# COLOR SENSOR
-color_sensor_left = ColorSensor('in1')
-assert color_sensor_left.connected, "Connect LEFT color sensor to sensor port 1"
-
-color_sensor_right = ColorSensor('in2')
-assert color_sensor_right.connected, "Connect RIGHT color sensor to sensor port 2"
-
-color_sensor_left.mode = 'COL-REFLECT'
-color_sensor_right.mode = 'COL-REFLECT'
-
-# BUTTON
-btn = Button()
-
-
 class LineFollowing:
 
     MOTORS = [2, 3]
@@ -36,7 +13,37 @@ class LineFollowing:
     def __init__(self, hidden_layer_sizes=(100, ), solver="lbfgs", learning_rate=0.0001, epochs=20000, batch_size=256):
         self.MLP = MLPRegressor(hidden_layer_sizes, solver=solver, learning_rate_init=learning_rate, shuffle=False,
                                 activation="logistic", max_iter=epochs, batch_size=batch_size, verbose=True)
-        pass
+
+        # Ev3 inputs
+        self.gs = None
+        self.motor_left = None
+        self.motor_right = None
+        self.color_sensor_left = None
+        self.color_sensor_right = None
+        self.btn = None
+
+    def setup_ev3(self):
+        # GYRO SENSOR
+        self.gs = GyroSensor()
+        assert self.gs.connected, "Connect a gyro sensor to any sensor port"
+        self.gs.mode = 'GYRO-ANG'
+
+        # MOTORS
+        self.motor_left = LargeMotor('outB')
+        self.motor_right = LargeMotor('outD')
+
+        # COLOR SENSOR
+        self.color_sensor_left = ColorSensor('in1')
+        assert self.color_sensor_left.connected, "Connect LEFT color sensor to sensor port 1"
+
+        self.color_sensor_right = ColorSensor('in2')
+        assert self.color_sensor_right.connected, "Connect RIGHT color sensor to sensor port 2"
+
+        self.color_sensor_left.mode = 'COL-REFLECT'
+        self.color_sensor_right.mode = 'COL-REFLECT'
+
+        # BUTTON
+        self.btn = Button()
 
     def train(self, train_csv, model_path="model/mlp.p"):
         data = np.genfromtxt(train_csv, delimiter=',', skip_header=1)
@@ -57,21 +64,23 @@ class LineFollowing:
         else:
             print("Warning: using unloaded model!")
 
-        while not btn.any():  # While no button is pressed.
-            left_color_intensity = color_sensor_left.value() / 100
-            right_color_intensity = color_sensor_right.value() / 100
+        self.setup_ev3()
+
+        while not self.btn.any():  # While no button is pressed.
+            left_color_intensity = self.color_sensor_left.value() / 100
+            right_color_intensity = self.color_sensor_right.value() / 100
 
             X = np.array([left_color_intensity, right_color_intensity])
 
             speed_left, speed_right = self.MLP.predict(X)
             print(left_color_intensity, right_color_intensity, speed_left, speed_right)
 
-            motor_left.run_forever(speed_sp=speed_left)
-            motor_right.run_forever(speed_sp=speed_right)
+            self.motor_left.run_forever(speed_sp=speed_left)
+            self.motor_right.run_forever(speed_sp=speed_right)
             sleep(0.01)
 
-        motor_left.stop(stop_action="hold")
-        motor_right.stop(stop_action="hold")
+        self.motor_left.stop(stop_action="hold")
+        self.motor_right.stop(stop_action="hold")
 
 if __name__ == '__main__':
     model = LineFollowing()
