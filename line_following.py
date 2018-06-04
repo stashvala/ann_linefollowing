2a8a158bb886b02d78c38a1aabcc389f117f6dec
stas <user.name>
2018-06-04 10:55:55 +0200

    run update, needs testing

--- line_following.py
+++ line_following.py
@@ -1,31 +1,31 @@
 import pickle
+from time import sleep
 
 import numpy as np
 from sklearn.neural_network import MLPRegressor
-from time import sleep
-# from ev3dev.ev3 import *
+from ev3dev.ev3 import *
 
 # GYRO SENSOR
-# gs = GyroSensor()
-# assert gs.connected, "Connect a gyro sensor to any sensor port"
-# gs.mode = 'GYRO-ANG'
-#
-# # MOTORS
-# motor_left = LargeMotor('outB')
-# motor_right = LargeMotor('outD')
-#
-# # COLOR SENSOR
-# color_sensor_left = ColorSensor('in1')
-# assert color_sensor_left.connected, "Connect LEFT color sensor to sensor port 1"
-#
-# color_sensor_right = ColorSensor('in2')
-# assert color_sensor_right.connected, "Connect RIGHT color sensor to sensor port 2"
-#
-# color_sensor_left.mode = 'COL-REFLECT'
-# # color_sensor_right.mode = 'COL-REFLECT'
-#
-# # BUTTON
-# btn = Button()
+gs = GyroSensor()
+assert gs.connected, "Connect a gyro sensor to any sensor port"
+gs.mode = 'GYRO-ANG'
+
+# MOTORS
+motor_left = LargeMotor('outB')
+motor_right = LargeMotor('outD')
+
+# COLOR SENSOR
+color_sensor_left = ColorSensor('in1')
+assert color_sensor_left.connected, "Connect LEFT color sensor to sensor port 1"
+
+color_sensor_right = ColorSensor('in2')
+assert color_sensor_right.connected, "Connect RIGHT color sensor to sensor port 2"
+
+color_sensor_left.mode = 'COL-REFLECT'
+color_sensor_right.mode = 'COL-REFLECT'
+
+# BUTTON
+btn = Button()
 
 
 class LineFollowing:
@@ -54,20 +54,26 @@ class LineFollowing:
     def run(self, model_path=None):
         if model_path is not None:
             self.MLP = pickle.load(open(model_path, "rb"))
+        else:
+            print("Warning: using unloaded model!")
 
         while not btn.any():  # While no button is pressed.
-            left_color_intensity = color_sensor_left.value()
+            left_color_intensity = color_sensor_left.value() / 100
+            right_color_intensity = color_sensor_right.value() / 100
 
-            X = np.array([left_color_intensity])
+            X = np.array([left_color_intensity, right_color_intensity])
 
             speed_left, speed_right = self.MLP.predict(X)
+            print(left_color_intensity, right_color_intensity, speed_left, speed_right)
 
-            motor_left.run_forever(speed_left)
-            motor_right.run_forever(speed_right)
+            motor_left.run_forever(speed_sp=speed_left)
+            motor_right.run_forever(speed_sp=speed_right)
             sleep(0.01)
 
+        motor_left.stop(stop_action="hold")
+        motor_right.stop(stop_action="hold")
 
 if __name__ == '__main__':
     model = LineFollowing()
     # model.train("data/electrical_tape.csv")
-    model.run("model/mlp")
+    model.run("model/mlp.p")
