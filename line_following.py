aa193ce5acbc205c0d87fd96a8eee33357cf4639
stas <user.name>
2018-06-14 18:09:14 +0200

    Deleting rows where both motors are zero, added new function for calculating ratio, doesn't work well as a feature

--- line_following.py
+++ line_following.py
@@ -14,19 +14,21 @@ class LineFollowing:
     MOTORS = [2, 3]
     COLOR_SENSORS = [4, 5]
     CONST_SPEED = 75
-    L_MOTOR_BASE = 261.0
-    R_MOTOR_BASE = 261.0
+    L_MOTOR_BASE = 299.0
+    R_MOTOR_BASE = 289.0
 
-    def __init__(self, hidden_layer_sizes=(6,), solver="adam", lr=0.00005, epochs=100, batch_size=32, alpha=0.01):
+    def __init__(self, hidden_layer_sizes=(8,), solver="adam", lr=0.00005, epochs=100, batch_size=64, alpha=0.001):
         self.MLP = MLPRegressor(hidden_layer_sizes, solver=solver, learning_rate_init=lr, alpha=alpha,
-                                learning_rate='adaptive', shuffle=False, activation="relu", max_iter=epochs,
+                                learning_rate='adaptive', shuffle=True, activation="relu", max_iter=epochs,
                                 batch_size=batch_size, validation_fraction=0.1, early_stopping=True, verbose=True)
 
         self.robot = None
 
     @staticmethod
     def motor_ratio(mot):
-        return ((mot[:, 0] + 1) / (mot[:, 1] + 1) - 1) / 1000
+        sign = (mot[:, 0] < mot[:, 1]).astype(int)
+        sign[sign == 0] = -1
+        return sign * (1 - (mot[:, 0] + 1) / (mot[:, 1] + 1))
 
     def train(self, train_csv, model_path="model/mlp.p"):
 
@@ -38,23 +40,29 @@ class LineFollowing:
         X = np.hstack((x_motors, x_color))
 
         y_motors = train_data[1:, self.MOTORS]  # shift motor outputs
-        y = y_motors  # shift motor outputs
+        y = y_motors
+
+        # remove rows where both motors are zero
+        ind = np.where(np.isclose(X[:, [0, 1]], np.zeros((1, 2))).all(axis=1))[0]
+        X = np.delete(X, ind, axis=0)
+        y = np.delete(y, ind, axis=0)
 
         # mirror data
-        X_mirror = X.copy()
-        X_mirror[:, [0, 1]] = X[:, [1, 0]]
-        X_mirror[:, [2, 3]] = X[:, [3, 2]]
-        y_mirror = y.copy()
-        y_mirror[:, [0, 1]] = y_mirror[:, [1, 0]]
+        # X_mirror = X.copy()
+        # X_mirror[:, [0, 1]] = X[:, [1, 0]]
+        # X_mirror[:, [2, 3]] = X[:, [3, 2]]
+        # y_mirror = y.copy()
+        # y_mirror[:, [0, 1]] = y_mirror[:, [1, 0]]
+        #
+        # X = np.vstack((X, X_mirror))
 
-        X = np.vstack((X, X_mirror))
         max_l_motor = np.max(X[:, 0])
         max_r_motor = np.max(X[:, 1])
         X[:, 0] /= max_l_motor
         X[:, 1] /= max_r_motor
         print("Max L motor = {}, Max R motor = {}".format(max_l_motor, max_r_motor))
 
-        y = np.vstack((y, y_mirror))
+        # y = np.vstack((y, y_mirror))
         y[:, 0] /= max_l_motor
         y[:, 1] /= max_r_motor
 
@@ -83,6 +91,8 @@ class LineFollowing:
 
         self.robot.drive(self.CONST_SPEED, self.CONST_SPEED)
 
+        print("Line following started")
+        print("Using base speeds: left motor {:3.2f}, right motor {:3.2f}".format(self.L_MOTOR_BASE, self.R_MOTOR_BASE))
         while not self.robot.btn.any():  # While no button is pressed.
             left_color_intensity = self.robot.color_sensor_left.value() / 100.
             right_color_intensity = self.robot.color_sensor_right.value() / 100.
@@ -114,6 +124,7 @@ class LineFollowing:
             SampleCollector.create_csv(out_log_file, out_log)
 
     def test(self, test_csv, model_path=None):
+        print("Testing model {} on dataset {}".format(model_path, test_csv))
         if model_path is not None:
             self.MLP = pickle.load(open(model_path, "rb"))
             print("Model {} loaded".format(model_path))
@@ -144,7 +155,7 @@ if __name__ == '__main__':
     print("Program started")
     model = LineFollowing()
 
-    model_path = "model/without_gyro.p"
+    model_path = "model/remove_zeros.p"
 
     # model.train("data/without_gyro.csv", model_path)
     # model.test("data/new_track.csv", model_path)
